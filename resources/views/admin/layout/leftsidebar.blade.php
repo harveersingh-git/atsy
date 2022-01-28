18e5563ad2a0455cd07dd9e37138ee2edcf686c6
Harveer Singh <harveersingh@virtualemployee.com>
2022-01-28 17:29:23 +0530

    redirec issue

--- resources/views/admin/layout/leftsidebar.blade.php
+++ resources/views/admin/layout/leftsidebar.blade.php
@@ -1,6 +1,6 @@
 <div id="left-sidebar" class="sidebar">
         <div class="navbar-brand">
-            <a href="index.html"><img src="{{asset('assets/images/icon-dark.svg')}}" alt="HexaBit Logo" class="img-fluid logo"><span>HexaBit</span></a>
+            <a href="{{ url('/') }}"><img src="{{asset('assets/images/icon-dark.svg')}}" alt="HexaBit Logo" class="img-fluid logo"><span>HexaBit</span></a>
             <button type="button" class="btn-toggle-offcanvas btn btn-sm btn-default float-right"><i class="lnr lnr-menu fa fa-chevron-circle-left"></i></button>
         </div>
         <div class="sidebar-scroll">
@@ -22,7 +22,7 @@
             </div>  
             <nav id="left-sidebar-nav" class="sidebar-nav">
                 <ul id="main-menu" class="metismenu">
-                    <li class="active"><a href="index.html"><i class="icon-home"></i><span>Dashboard</span></a></li>
+                    <li class="active"><a href="{{ url('/') }}"><i class="icon-home"></i><span>Dashboard</span></a></li>
                    
                     <li>
                         <a href="#uiElements" class="has-arrow"><i class="icon-settings"></i><span>Settings</span></a>
